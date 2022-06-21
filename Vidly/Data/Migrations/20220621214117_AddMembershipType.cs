d9199db480650302c25d64ae471b060941676719
ChrisShocker <fallenchris@live.com>
2022-06-21 14:44:26 -0700

    Create class to track Customer membership type

---
+++ Vidly/Data/Migrations/20220621214117_AddMembershipType.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vidly.Data.Migrations
+{
+    public partial class AddMembershipType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte>(
+                name: "MemberShipTypeId",
+                table: "Customers",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0);
+
+            migrationBuilder.CreateTable(
+                name: "MembershipType",
+                columns: table => new
+                {
+                    Id = table.Column<byte>(type: "tinyint", nullable: false),
+                    SignupFee = table.Column<short>(type: "smallint", nullable: false),
+                    DuationInMonths = table.Column<byte>(type: "tinyint", nullable: false),
+                    DiscountRate = table.Column<byte>(type: "tinyint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MembershipType", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_MemberShipTypeId",
+                table: "Customers",
+                column: "MemberShipTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customers_MembershipType_MemberShipTypeId",
+                table: "Customers",
+                column: "MemberShipTypeId",
+                principalTable: "MembershipType",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customers_MembershipType_MemberShipTypeId",
+                table: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "MembershipType");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Customers_MemberShipTypeId",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "MemberShipTypeId",
+                table: "Customers");
+        }
+    }
+}
---
+++ Vidly/Models/MembershipType.cs
@@ -0,0 +1,10 @@
+﻿namespace Vidly.Models
+{
+    public class MembershipType
+    {
+        public byte Id { get; set; }
+        public short SignupFee { get; set; }
+        public byte DuationInMonths { get; set; }
+        public byte DiscountRate { get; set; }
+    }
+}
