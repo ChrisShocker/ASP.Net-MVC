a0839978ce99c83632625fb78af8f631f16b5e8f
ChrisShocker <fallenchris@live.com>
2022-06-21 12:30:21 -0700

    Add Customer search by id

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -6,26 +6,66 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
-        public IActionResult List()
+
+        public IActionResult List(int Id)
         {
             //Create some customers to pass to the View
             var customers = new List<Customer>
             {
-                new Customer { Name = "Gerald" },
-                new Customer { Name = "Harry"}
+                new Customer { Name = "Gerald", Id = 1 },
+                new Customer { Name = "Harry", Id = 2}
             };
 
 
-
-            //create a viewModel object that will hold the customers
             var viewModel = new CustomersViewModel
             {
                 Customers = customers
             };
 
+            //If an Id is passed in
+            if (Id != 0)
+            {
+                var uniqueCustomer = new List<Customer>();
+                foreach (var customer in customers)
+                {
+                    if (customer.Id == Id)
+                        uniqueCustomer.Add(customer);
+                }
 
-            //Pass the viewModel to the View for rendering
-            return View(viewModel);
+
+                //if the customer exists
+                if (uniqueCustomer.Count > 0)
+                {
+                    //create a viewModel object that will hold the customers
+                    var viewModelUnique = new CustomersViewModel
+                    {
+                        Customers = uniqueCustomer
+                    };
+
+                    //Pass the viewModel to the View for rendering
+                    return View(viewModelUnique);
+                }
+
+                //customer not found
+                else
+                {
+                    var notFound = new List<Customer>
+                    {
+                        new Customer { Name = "Customer Not Found"}
+                    };
+
+                    var NotFoundViewModel = new CustomersViewModel
+                    {
+                        Customers = notFound
+                    };
+
+                    return View(NotFoundViewModel);
+                }
+            }
+
+            //No Arguments passed in return the current list of customers
+            else
+                return View(viewModel);
         }
     }
 }
