374f425524d26afd0ca4f2e2114d9195118eb1de
ChrisShocker <fallenchris@live.com>
2022-06-21 12:00:27 -0700

    Create controller to handle Customer requests

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public IActionResult List()
+        {
+            //Create some customers to pass to the View
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Gerald" },
+                new Customer { Name = "Harry"}
+            };
+
+
+
+            //create a viewModel object that will hold the customers
+            var viewModel = new CustomersViewModel
+            {
+                Customers = customers
+            };
+
+
+            //Pass the viewModel to the View for rendering
+            return View(viewModel);
+        }
+    }
+}
