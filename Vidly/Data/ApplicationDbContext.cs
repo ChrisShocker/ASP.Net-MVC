9613dae60c8ad0850a58e7f0d8cbb271c5c9f95a
ChrisShocker <fallenchris@live.com>
2022-06-23 14:43:47 -0700

    Add Movie Model to database

--- Vidly/Data/ApplicationDbContext.cs
+++ Vidly/Data/ApplicationDbContext.cs
@@ -7,6 +7,7 @@ namespace Vidly.Data
     public class ApplicationDbContext : IdentityDbContext
     {
         public DbSet<Customer>? Customers { get; set; }
+        public DbSet<Movie>? Movies { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
