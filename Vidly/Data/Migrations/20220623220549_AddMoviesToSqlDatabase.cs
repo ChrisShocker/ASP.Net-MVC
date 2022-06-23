0284b19409b26431e9a04722b1411b4bf475e4eb
ChrisShocker <fallenchris@live.com>
2022-06-23 16:16:18 -0700

    Create Genre Parameter inside Movie Model

---
+++ Vidly/Data/Migrations/20220623220549_AddMoviesToSqlDatabase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vidly.Data.Migrations
+{
+    public partial class AddMoviesToSqlDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Insert into Movies(Title, Genre) VALUES('Die Hard','Action')");
+            migrationBuilder.Sql("Insert into Movies(Title, Genre) VALUES('Die Hard 2','Action')");
+            migrationBuilder.Sql("Insert into Movies(Title, Genre) VALUES('Terminator','Action')");
+            migrationBuilder.Sql("Insert into Movies(Title, Genre) VALUES('Halo','Action')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Vidly/Data/Migrations/20220623222823_LinkGenreAndMoviesModels.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vidly.Data.Migrations
+{
+    public partial class LinkGenreAndMoviesModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Movies_Genre_GenreId",
+                table: "Movies");
+
+            migrationBuilder.DropTable(
+                name: "Genre");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Movies_GenreId",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "GenreId",
+                table: "Movies");
+        }
+    }
+}
