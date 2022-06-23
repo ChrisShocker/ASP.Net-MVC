ad53dfa3b7422d4f0f4bef5a4e35d7b6be72aac1
ChrisShocker <fallenchris@live.com>
2022-06-23 16:17:39 -0700

    Create Genre Table entries

---
+++ Vidly/Data/Migrations/20220623225723_PopulateGenreTable.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vidly.Data.Migrations
+{
+    public partial class PopulateGenreTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Insert into Genre(Name) VALUES('Action')");
+            migrationBuilder.Sql("Insert into Genre(Name) VALUES('Comedy')");
+            migrationBuilder.Sql("Insert into Genre(Name) VALUES('Romance')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
