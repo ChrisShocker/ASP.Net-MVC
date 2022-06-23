8044f33e7d6ce5bce9d080f611e5a408a8a67a98
ChrisShocker <fallenchris@live.com>
2022-06-23 11:20:15 -0700

    Add data validation to customer Name

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,9 +1,22 @@
-﻿namespace Vidly.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
 {
     public class Customer
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
+
+        /*
+         * Data Annoations
+         * - allow us to add data validation to types
+         * - MVC automatically enforces and displays proper messages to clients
+         * 
+         * https://docs.microsoft.com/en-us/aspnet/mvc/overview/older-versions/mvc-music-store/mvc-music-store-part-6
+         */
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
         public bool IsSubscribedToNewsLetter { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MemberShipTypeId { get; set; }
