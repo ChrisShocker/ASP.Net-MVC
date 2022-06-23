26b287901dff5c3a4579537a63f5e3069259c031
ChrisShocker <fallenchris@live.com>
2022-06-23 16:30:42 -0700

    Add Movie parameters

---
+++ Vidly/Data/Migrations/20220623233007_AddMovieParameters.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vidly.Data.Migrations
+{
+    public partial class AddMovieParameters : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Stock",
+                table: "Movies",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "dateAdded",
+                table: "Movies",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "releaseDate",
+                table: "Movies",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Stock",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "dateAdded",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "releaseDate",
+                table: "Movies");
+        }
+    }
+}
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -5,6 +5,9 @@
         public int Id { get; set; }
         public string Title { get; set; }
         public Genre Genre { get; set; }
+        public DateTime releaseDate { get; set; }
+        public DateTime dateAdded { get; set; }
+        public int Stock { get; set; }
 
         public Movie()
         {
