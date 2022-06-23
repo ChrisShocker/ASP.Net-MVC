cfb1a028e60ac3c62e487b91a2e17a4342b20ebe
ChrisShocker <fallenchris@live.com>
2022-06-23 13:45:42 -0700

    Add birthdate values to Customer Table

---
+++ Vidly/Data/Migrations/20220623202729_UpdateCustomerBirthDates.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vidly.Data.Migrations
+{
+    public partial class UpdateCustomerBirthDates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var dateTime1 = new DateTime(1990, 10, 20);
+            migrationBuilder.Sql("UPDATE Customers SET BirthDate = '2000/12/8' WHERE Id=1");
+            migrationBuilder.Sql("UPDATE Customers SET BirthDate = '1990/8/4' WHERE Id=2");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
