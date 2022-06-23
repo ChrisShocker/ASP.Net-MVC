6c8241cc16a49e07c7a5587ddbbbe645be34860f
ChrisShocker <fallenchris@live.com>
2022-06-23 12:13:46 -0700

    Add Eager Loading Customer and MembershipType

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Vidly.Data;
 using Vidly.Models;
 using Vidly.ViewModels;
@@ -22,11 +23,20 @@ namespace Vidly.Controllers
             _context.Dispose();
         }
 
+        /*
+         * Eager Loading
+         * - By default Entity won't load related objects ie, MembershipType isn't loaded and will through an error
+         *   even though it's a related object to Customer
+         *   
+         * We can load related queryr objects by Including a reference to the related object ie, 'Include(c => c.MembershipType)'
+         *
+         * https://docs.microsoft.com/en-us/ef/core/querying/related-data/eager
+         */
 
         public IActionResult List()
         {
             //Entity defers the query until the customer object is actually iterated over ie, at View(customers)
-            var customers = _context.Customers;
+            var customers = _context.Customers.Include(c => c.MembershipType);
 
             return View(customers);
         }
