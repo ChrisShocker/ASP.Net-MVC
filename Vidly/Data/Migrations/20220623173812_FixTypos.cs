d991d55c68f03c5bcfc13fed1c0bbdc00091e3b2
ChrisShocker <fallenchris@live.com>
2022-06-23 10:39:55 -0700

    Fix DB typos

---
+++ Vidly/Data/Migrations/20220623173812_FixTypos.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vidly.Data.Migrations
+{
+    public partial class FixTypos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SignupFee",
+                table: "MembershipType",
+                newName: "SignUpFee");
+
+            migrationBuilder.RenameColumn(
+                name: "DuationInMonths",
+                table: "MembershipType",
+                newName: "DurationInMonths");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SignUpFee",
+                table: "MembershipType",
+                newName: "SignupFee");
+
+            migrationBuilder.RenameColumn(
+                name: "DurationInMonths",
+                table: "MembershipType",
+                newName: "DuationInMonths");
+        }
+    }
+}
