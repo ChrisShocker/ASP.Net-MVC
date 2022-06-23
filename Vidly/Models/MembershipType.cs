7930b3f22429c2406b41f38b30c9a447d84229fe
ChrisShocker <fallenchris@live.com>
2022-06-23 10:39:03 -0700

    Fix typos

--- Vidly/Models/MembershipType.cs
+++ Vidly/Models/MembershipType.cs
@@ -3,8 +3,8 @@
     public class MembershipType
     {
         public byte Id { get; set; }
-        public short SignupFee { get; set; }
-        public byte DuationInMonths { get; set; }
+        public short SignUpFee { get; set; }
+        public byte DurationInMonths { get; set; }
         public byte DiscountRate { get; set; }
     }
 }
