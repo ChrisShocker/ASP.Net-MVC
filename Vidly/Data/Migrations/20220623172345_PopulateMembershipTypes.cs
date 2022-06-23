e4ca0fff060d8b75bc453f25e8062b4d5ec03b0b
ChrisShocker <fallenchris@live.com>
2022-06-23 10:39:38 -0700

    Populate membership types

---
+++ Vidly/Data/Migrations/20220623172345_PopulateMembershipTypes.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vidly.Data.Migrations
+{
+    public partial class PopulateMembershipTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            //Fist Membership type:
+            migrationBuilder.Sql("Insert into MembershipType(Id, SignUpFee, DurationInMonths, DiscountRate) VALUES(1,0,0,0)");
+            //Second Membership type:
+            migrationBuilder.Sql("Insert into MembershipType(Id, SignUpFee, DurationInMonths, DiscountRate) VALUES(2,30,1,10)");
+            //Thrid Membership type:
+            migrationBuilder.Sql("Insert into MembershipType(Id, SignUpFee, DurationInMonths, DiscountRate) VALUES(3,90,3,15)");
+            //Fourth Membership type:
+            migrationBuilder.Sql("Insert into MembershipType(Id, SignUpFee, DurationInMonths, DiscountRate) VALUES(4,300,12,20)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
