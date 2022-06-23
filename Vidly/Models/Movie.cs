f512d394e7432d0185e642aff722717a3a391e21
ChrisShocker <fallenchris@live.com>
2022-06-23 16:53:39 -0700

    Correct parameter names

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -5,8 +5,8 @@
         public int Id { get; set; }
         public string Title { get; set; }
         public Genre Genre { get; set; }
-        public DateTime releaseDate { get; set; }
-        public DateTime dateAdded { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public DateTime DateAdded { get; set; }
         public int Stock { get; set; }
 
         public Movie()
